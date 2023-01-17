053f86e2487e5a5db6e0f0a37096833d90c0ac3c
Hunter Marshall <beidah@gmail.com>
2023-01-16 23:05:22 -0600

    Add NextAuth.js as authorization library

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,31 @@
-type Props = {};
+import { GetServerSidePropsContext } from "next"
+import { BuiltInProviderType } from "next-auth/providers"
+import { ClientSafeProvider, getProviders, LiteralUnion, signIn } from "next-auth/react"
+import Layout from "../components/Layout"
 
-const Login = (props: Props) => {
-  return <div>Login Page</div>;
-};
+type Props = {
+  providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider>
+}
 
-export default Login;
+export default function Login({ providers }: Props) {
+  return (
+    <Layout>
+      {/* Loop through the different providers and make a login button for each */}
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const providers = await getProviders();
+  console.log({ providers });
+  return {
+    props: { providers },
+  }
+}
