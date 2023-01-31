92300ab9cc9b505ae200dbc8ed50879e726e202f
RMcGuire <28935544+rmcguire6@users.noreply.github.com>
2023-01-30 17:57:12 -0800

    feat: AddItem button

--- src/pages/yard/add-item.tsx
+++ src/pages/yard/add-item.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import Layout from '../../components/Layout';
+import React from "react";
+import Router from "next/router";
+import Layout from "../../components/Layout";
 
-type Props = {};
-
-const AddItem = (props: Props) => {
+const AddItem: React.FC = () => {
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const title = "Samsung Galaxy tablet";
+    const description = "Lorem ipsum dolor sit amet consectetur adipisic";
+    const picture = "Image not available";
+    const price = 84;
+    const category = "Electronics";
+    try {
+      const body = { title, description, picture, price, category };
+      await fetch("/api/item", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      await Router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
       <div>Add Item Page</div>
+      <button
+        className="btn btn-primary"
+        type="submit"
+        onClick={(e) => submitData(e)}
+      >
+        Add Item
+      </button>
     </Layout>
   );
 };
