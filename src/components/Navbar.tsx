159713b607381c814b159029e0886a9234b4b3a9
Laura <lmcsay@gmail.com>
2023-02-01 16:45:12 -0600

    Add basic style to nav and create add item for logged in users

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,33 @@
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const id = session?.user?.id;
+  const router = useRouter();
+  const isAdding = router.pathname.includes('add-item');
+  const home = session ? `/yard/${id}` : '/';
 
   return (
-    <nav>
-      {session ? (
-        <button onClick={() => signOut()}>
-          <a>Log out</a>
-        </button>
-      ) : (
-        <Link href='/login'>Sign In</Link>
-      )}
+    <nav className="h-16">
+      <div className="nav-center flex justify-between h-full items-center mx-10">
+        <div className="logo">
+          <a href={home}>Yard Sale</a>
+        </div>
+        <div className="nav-links flex justify-between space-x-8">
+          {session ? (
+            <>
+              {!isAdding && (
+                <Link href={`/yard/${id}/item/add-item`}>Add Item</Link>
+              )}
+              <button onClick={() => signOut()}>Log out</button>
+            </>
+          ) : (
+            <Link href="/login">Sign In</Link>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
