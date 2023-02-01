5ae13abad3f868d9cab421441fbc0d58d822feda
RMcGuire <28935544+rmcguire6@users.noreply.github.com>
2023-02-01 09:50:04 -0800

    bug: Forgot prisma import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import { prisma } from "../server/db";
 import Layout from "../components/Layout";
 import Item from "../components/Item";
 import { ItemType } from "../components/Item";
