a57af909a92d67e5767b14a71a497d8b18afad9a
Laura <lmcsay@gmail.com>
2023-02-08 13:57:44 -0600

    Use react-select for mobile friendly contact form select

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { ItemType } from './Item';
 import { User } from '@prisma/client';
 import Router from 'next/router';
+import Select from 'react-select';
 
 type FormProps = {
   user: User;
@@ -13,21 +14,15 @@ const ContactForm = ({ user, items: sellerItems }: FormProps) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(null);
 
   const options = sellerItems.map((item) => {
     return { value: item.title, label: item.title };
   });
 
-  const handleChangeSelect = (e) => {
-    const updatedOptions = [...e.target.options]
-      .filter((option) => option.selected)
-      .map((x) => x.value);
-    setItems(updatedOptions);
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const itemNames = items?.map((item) => item.value);
     try {
       axios.defaults.headers.post['Content-Type'] = 'application/json';
       await axios
@@ -35,14 +30,14 @@ const ContactForm = ({ user, items: sellerItems }: FormProps) => {
           name,
           email,
           message,
-          items,
+          itemNames,
         })
         .then((response) => console.log(response))
         .catch((error) => console.log(error));
       setName('');
       setEmail('');
       setMessage('');
-      setItems([]);
+      setItems(null);
       Router.push('/');
     } catch (error) {
       console.error(error);
@@ -56,8 +51,8 @@ const ContactForm = ({ user, items: sellerItems }: FormProps) => {
           Contact Form
         </h3>
         <p className='mb-4 px-8'>
-          Let the seller know what you're interested in and they'll get back to
-          you!
+          Let the seller know what you&#39;re interested in and they&#39;ll get
+          back to you!
         </p>
         <form onSubmit={(e) => handleSubmit(e)}>
           <input
@@ -85,27 +80,17 @@ const ContactForm = ({ user, items: sellerItems }: FormProps) => {
           ></textarea>
           <div className='flex-col w-8/12 mx-auto'>
             <div>
-              <label htmlFor='yard-items'>Select item(s) of interest:</label>
+              <label htmlFor='yard-items mb-2'>
+                Select item(s) of interest:
+              </label>
             </div>
-            <select
-              className='divide-y-2 mt-2 w-52'
-              onChange={handleChangeSelect}
-              multiple
-              name='yard-items'
-              value={items}
-            >
-              {options.map((item) => {
-                return (
-                  <option
-                    className='px-2 py-1'
-                    key={item.label}
-                    value={item.value}
-                  >
-                    {item.label}
-                  </option>
-                );
-              })}
-            </select>
+            <Select
+              className='mt-4'
+              isMulti
+              defaultValue={items}
+              onChange={setItems}
+              options={options}
+            />
           </div>
           <button className='block mx-auto btn btn-primary mt-10' type='submit'>
             Submit
