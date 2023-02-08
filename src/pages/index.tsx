34914a0c56cd2c0f4c413515d8a2293705ca5358
Laura <lmcsay@gmail.com>
2023-02-08 12:34:25 -0600

    Filter out sold items from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,13 @@ const Search: React.FC<Props> = (props) => {
     <Layout>
       <section className='item-section'>
         <div className='item-section-center'>
-          {props.items.map((singleItem) => (
-            <div key={singleItem.itemId} className='flex justify-center'>
-              <Item item={singleItem} />
-            </div>
-          ))}
+          {props.items
+            .filter((singleItem) => singleItem.status !== 'sold')
+            .map((singleItem) => (
+              <div key={singleItem.itemId} className='flex justify-center'>
+                <Item item={singleItem} />
+              </div>
+            ))}
         </div>
       </section>
     </Layout>
