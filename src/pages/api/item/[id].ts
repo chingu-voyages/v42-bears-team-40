7480c406fdc91c97764a0315b042827079a9346d
RMcGuire <28935544+rmcguire6@users.noreply.github.com>
2023-02-07 16:22:11 -0800

    feat: Edit item

--- src/pages/api/item/[id].ts
+++ src/pages/api/item/[id].ts
@@ -1,10 +1,12 @@
-import { prisma } from "../../../server/db";
+// import { prisma } from '../../../server/db';
 
 // PUT api/item/:id
 // Update Item
 export default async function editItem(req, res) {
   const itemId = req.query.id;
-  const { title, description, picture, price, category } = req.body;
+  const { title, description, picture, price, category, status } = req.body;
+  console.log('from backend itemId', itemId);
+
   const item = await prisma.item.update({
     where: { itemId: itemId },
     data: {
@@ -13,6 +15,7 @@ export default async function editItem(req, res) {
       picture,
       price,
       category,
+      status,
     },
   });
   res.json(item);
