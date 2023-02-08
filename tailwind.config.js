16160026461eac68daaeaca5bc1f9b7a422ec868
Laura <lmcsay@gmail.com>
2023-02-07 19:55:23 -0600

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-green': '#004643',
+        'light-green': '#abd1c6',
+        'white-ish': '#fffffe',
+        'gray-ish': '#e8e4e6',
+        'custom-yellow': '#f9bc60',
+        'custom-purple': '#7f5af0',
+      },
+    },
   },
   plugins: [],
-}
+};
