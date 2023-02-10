172f570e6e012fc7e73a4af0ebde5ba21c95f9eb
Laura <lmcsay@gmail.com>
2023-02-10 15:27:35 -0600

    Display nav hamburger button for signed in users on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
+import { SetStateAction, Dispatch } from 'react';
 import Link from 'next/link';
-import { signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { signOut, useSession } from 'next-auth/react';
 
-const Navbar = () => {
+type NavProps = {
+  handleToggle: Dispatch<SetStateAction<boolean>>;
+};
+
+const Navbar = ({ handleToggle }: NavProps) => {
   const { data: session } = useSession();
   const id = session?.user?.id;
   const router = useRouter();
@@ -11,20 +16,21 @@ const Navbar = () => {
 
   return (
     <nav className='h-24 bg-dark-green text-gray-50 tracking-wide md:text-lg drop-shadow-lg'>
-      <div className='nav-center flex justify-between h-full items-center mx-6 md:mx-10'>
-        <div className='logo hover:text-custom-yellow flex items-center'>
+      <div className='nav-center flex justify-between h-full items-center mx-12'>
+        <div className='logo flex'>
           <Link href='/'>
             <Image
-              src='/images/yard-sale.png'
+              src='/images/yard-sale-grass-1.png'
               alt='logo'
-              height={150}
-              width={150}
+              height={100}
+              width={100}
+              className='justify-self-start'
             />
           </Link>
         </div>
-        <div className='nav-links flex justify-between space-x-4 md:space-x-8'>
-          {session ? (
-            <>
+        {session ? (
+          <>
+            <div className='nav-links hidden md:flex justify-between space-x-4 md:space-x-8 '>
               <Link className='hover:text-custom-yellow' href={`/yard/${id}`}>
                 Profile
               </Link>
@@ -42,13 +48,19 @@ const Navbar = () => {
               >
                 Log out
               </button>
-            </>
-          ) : (
-            <Link className='hover:text-custom-yellow' href='/login'>
-              Sign In
-            </Link>
-          )}
-        </div>
+            </div>
+            <button onClick={() => handleToggle(true)} className='md:hidden'>
+              <img
+                src='/images/toggle.png'
+                className='h-10 hover:scale-105'
+              ></img>
+            </button>
+          </>
+        ) : (
+          <Link className='hover:text-custom-yellow' href='/login'>
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
