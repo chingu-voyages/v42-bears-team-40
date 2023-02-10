a71819fc78bf7ed739bfef6be94192a93cadbd86
Laura <lmcsay@gmail.com>
2023-02-10 15:28:45 -0600

    Add toggle nav links for signed in mobile users

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,48 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { ReactNode } from 'react';
-import Head from 'next/head';
+import NavLinks from './NavLinks';
 
 type LayoutProps = { children: ReactNode };
 
 const Layout = ({ children }: LayoutProps) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { data: session } = useSession();
+
   return (
     <>
       <Head>
         <title>Yard Sale</title>
       </Head>
-      <Navbar />
+      <Navbar handleToggle={setIsSidebarOpen} />
       <div className='bg-light-green min-height'>{children}</div>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='sidebar-content'>
+          <button
+            onClick={() => setIsSidebarOpen(false)}
+            className='absolute top-3 left-3'
+          >
+            <img src='/images/close.png' className='h-8' />
+          </button>
+          <header>
+            <Image
+              src='/images/yard-sale-grass.png'
+              alt='logo'
+              height={200}
+              width={200}
+            />
+          </header>
+          <NavLinks id={session?.user.id} handleToggle={setIsSidebarOpen} />
+        </div>
+      </div>
       <Footer />
     </>
   );
