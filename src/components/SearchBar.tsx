00d12a51ddd379f713e386f2b13859f2ca6833aa
Laura <lmcsay@gmail.com>
2023-02-12 15:44:44 -0600

    Fix search mobile styling issues

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,19 +11,21 @@ const SearchBar = ({ handleSearch }) => {
   return (
     <div>
       <form onSubmit={(e) => handleChange(e)}>
-        <div className='mx-auto my-2 text-center w-full flex'>
-          <input
-            className='m-0 p-2 border grow h-10 border-dark-green rounded-lg rounded-r-none'
-            onChange={(e) => setSearchText(e.target.value)}
-            type='text'
-            value={searchText}
-            name='searchText'
-          />
-          <button className='rounded-r-lg bg-dark-green py-2 px-4 tracking-wide text-white hover:bg-emerald-800 inline-block h-10 mr-2 md:mr-1'>
-            Search
-          </button>
+        <div className='mx-auto my-2 text-center w-full md:flex'>
+          <div className='flex'>
+            <input
+              className='m-0 p-2 border grow h-10 border-dark-green rounded-lg rounded-r-none'
+              onChange={(e) => setSearchText(e.target.value)}
+              type='text'
+              value={searchText}
+              name='searchText'
+            />
+            <button className='rounded-r-lg bg-dark-green py-2 px-3 tracking-wide text-white hover:bg-emerald-800 inline-block h-10 md:mr-1'>
+              Search
+            </button>
+          </div>
           <button
-            className='my-1 md:mr-4 tracking-wide w-28 text-dark-green hover:underline'
+            className='my-1 md:mr-4 tracking-wide w-28 text-dark-green hover:underline inline-block'
             onClick={(e) => setSearchText('')}
           >
             Clear Search
